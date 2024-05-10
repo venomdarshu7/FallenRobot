4a119a75a9eb3fdc38e172a457db0796cb45c77d
venomdarshu7 <138948660+venomdarshu7@users.noreply.github.com>
2024-05-10 15:47:16 +0530

    Update config.py
    Signed-off-by: venomdarshu7 <138948660+venomdarshu7@users.noreply.github.com>

--- FallenRobot/config.py
+++ FallenRobot/config.py
@@ -5,20 +5,20 @@ class Config(object):
     API_ID = 20738979
     API_HASH = "a6d015153068a35390a336fe0a38dd64"
 
-    CASH_API_KEY = ""  # Get this value for currency converter from https://www.alphavantage.co/support/#api-key
+    CASH_API_KEY = "EWW2T30R2NSCYVC8"  # Get this value for currency converter from https://www.alphavantage.co/support/#api-key
 
     DATABASE_URL = "postgres://euacssdp:ExVTYiLLq8fEtG6vkTNO805DrYdbSjn9@jelani.db.elephantsql.com/euacssdp"  # A sql database url from elephantsql.com
 
     EVENT_LOGS = (-1001586622410)  # Event logs channel to note down important bot level events
 
-    MONGO_DB_URI = "mongodb+srv://lazyprinces:darshu567@cluster0.mwgkkci.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"  # Get ths value from cloud.mongodb.com
+    MONGO_DB_URI = "mongodb+srv://das:darshu567@cluster0.mwgkkci.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"  # Get ths value from cloud.mongodb.com
 
     # Telegraph link of the image which will be shown at start command.
     START_IMG = "https://telegra.ph/file/a1489375c10e1f70156e0.jpg"
 
     SUPPORT_CHAT = "vdsamr"  # Your Telegram support group chat username where your users will go and bother you
 
-    TOKEN = "6066024987:AAFaqlfZHQdVWvbKhDolsAFe85m_d8KZPfA"  # Get bot token from @BotFather on Telegram
+    TOKEN = "6472392988:AAGczAHFlx_U2Fh-LkzrjMT6sGJCniCSWwA"  # Get bot token from @BotFather on Telegram
 
     TIME_API_KEY = "http://api.timezonedb.com/v2.1/list-time-zone"  # Get this value from https://timezonedb.com/api
 
